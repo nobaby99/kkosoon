831e6709b459636db11d9437b32a1be1b9a85028
nobaby99 <80159991+nobaby99@users.noreply.github.com>
2021-09-10 20:18:49 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1486,8 +1486,8 @@ class mainCog(commands.Cog):
 					color=0xff00ff
 					)
 			embed.add_field(
-					name="----- Special Thanks to. -----",
-					value= '```총무, 옹님, 공부중, 꽃신, 별빛, 크마, D.H.Kim, K.H.Sim, 쿠쿠, 오브로드, D.H.Oh, Bit, 팥빵, 천려, 이파리, 도미, 일깡, B.Park```'
+					name="----- Specs to. -----",
+					value= '```총무, 옹님, 공부중, 꽃신, 별빛, 크마, D.H.Kim, K.H.Sim, 쿠쿠, 오브로드, D.H.Oh, Bit, 팥빵, 천려, 이파리, 도미, 일깡, B.Park, 꼬순```'
 					)
 			await ctx.send(embed=embed, tts=False)
 		else:
